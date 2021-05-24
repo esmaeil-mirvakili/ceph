0d4c624b2da36ab6d479ee096af0dcaafa3d7b22
Esmaeil <smirvaki@ucsc.edu>
2021-05-23 21:07:03 -0700

    sf_codel v3

--- src/common/CoDelAdaptiveTarget.cc
+++ src/common/CoDelAdaptiveTarget.cc
@@ -73,11 +73,11 @@ void CoDel::_coarse_interval_process() {
     int64_t time = 0;
     if (!mono_clock::is_zero(slow_interval_start)) {
         time = std::chrono::nanoseconds(now - slow_interval_start).count();
-        time = time / 1000000.0;    // to ms
+//        time = time / 1000000.0;    // to ms
         cur_throughput = (coarse_interval_size * 1.0) / time;
         avg_lat = (sum_latency * 1.0) / txc_cnt;
-        auto cur_loss = pow(avg_lat, 0.2) / cur_throughput;
-        auto pre_loss = pow(slow_interval_lat, 0.2) / slow_interval_throughput;
+        auto cur_loss = pow(avg_lat, 0.5) / cur_throughput;
+        auto pre_loss = pow(slow_interval_lat, 0.5) / slow_interval_throughput;
         if (slow_interval_throughput > 0){
             delta = -(learning_rate * (cur_loss - pre_loss))/(avg_lat - slow_interval_lat);
             if (target_latency + delta >= min_target_latency && target_latency + delta <= max_target_latency)
