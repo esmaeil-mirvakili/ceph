9df2ca0d38f16f2afe8d1e6b9a53c2442940ccce
Esmaeil <smirvaki@ucsc.edu>
2021-07-12 12:35:10 -0700

    SFCoDelFPI

--- src/common/CoDelAdaptiveTargetFPI.cc
+++ src/common/CoDelAdaptiveTargetFPI.cc
@@ -19,10 +19,10 @@ CoDel::~CoDel() {
 void CoDel::initialize(int64_t init_interval, int64_t init_target, bool adaptive, bool active) {
     initial_interval = init_interval;
     initial_target_latency = init_target;
+    target_latency = min_target_latency;
+    reconfigure_phase = true;
     adaptive_target = adaptive;
     activated = active;
-    fx_size = (max_target_latency - min_target_latency) / step_size;
-    throughput_target_fx = new ThroughputInfo[fx_size];
     {
         std::lock_guard l1{fast_timer_lock};
         fast_timer.cancel_all_events();
@@ -35,22 +35,6 @@ void CoDel::initialize(int64_t init_interval, int64_t init_target, bool adaptive
     _coarse_interval_process();
 }
 
-int64_t CoDel::get_index() {
-    int index = selected_target_index + selected_target_offset;
-    return index;
-}
-
-int64_t CoDel::calculate_target() {
-    return min_target_latency + (get_index() * step_size);
-}
-
-double_t CoDel::calculate_throughput(ThroughputInfo throughputInfo) {
-    double_t sum = 0;
-    for (unsigned int i = 0; i < throughputInfo.throughput_vec.size(); i++)
-        sum += throughputInfo.throughput_vec[i];
-    return sum / (throughputInfo.throughput_vec.size() * );
-}
-
 void CoDel::register_queue_latency(int64_t latency, double_t throttle_usage, int64_t size) {
     std::lock_guard l(register_lock);
     if (min_latency == INT_NULL || latency < min_latency) {
@@ -59,13 +43,6 @@ void CoDel::register_queue_latency(int64_t latency, double_t throttle_usage, int
     sum_latency += latency;
     slow_interval_txc_cnt++;
     coarse_interval_size += size;
-}
-
-void CoDel::re_configure(int offset) {
-    reconfigure = true;
-    selected_target_offset = offset;
-    if (get_index() >= 0 && get_index() <= fx_size - 1)
-        throughput_target_fx[get_index()].throughput_vec.clear();
 }
 
 void CoDel::_interval_process() {
@@ -104,80 +81,39 @@ void CoDel::_coarse_interval_process() {
     std::lock_guard l(register_lock);
     mono_clock::time_point now = mono_clock::now();
 
-    double_t time = std::chrono::nanoseconds(now - slow_interval_start).count();
-    time = time / (1000 * 1000 * 1000.0);
+    if (!mono_clock::is_zero(slow_interval_start) && slow_interval_txc_cnt > 0) {
+        double_t time = std::chrono::nanoseconds(now - slow_interval_start).count();
+        time = time / (1000 * 1000 * 1000.0);
+        double_t cur_throughput = (coarse_interval_size * 1.0) / time;
+        cur_throughput = cur_throughput / 1024;
+        cur_throughput = cur_throughput / 1024;
 
-    double_t cur_throughput = (coarse_interval_size * 1.0) / time;
-    cur_throughput = cur_throughput / 1024;
-    cur_throughput = cur_throughput / 1024;
-
-//    double_t avg_lat = (sum_latency / (1000 * 1000.0)) / slow_interval_txc_cnt;
-    if (get_index() < 0)
-        re_configure(1);
-    if (get_index() > fx_size - 1)
-        selected_target_offset = 0;
-    throughput_target_fx[get_index()].throughput_vec.push_back(cur_throughput);
-    if (throughput_target_fx[get_index()].throughput_vec.size() > sliding_window_size) {
-        auto begin = throughput_target_fx[get_index()].throughput_vec.begin();
-        throughput_target_fx[get_index()].throughput_vec.erase(begin);
-
-        if (reconfigure) {
-            if (selected_target_offset < 0)
-                re_configure(1);
-            else
-                selected_target_offset = 0;
-        } else {
-            double_t throughput = calculate_throughput(throughput_target_fx[get_index()]);
-            double_t prev_throughput = 0;
-            double_t prev_slope = 0;
-            double_t next_throughput = 0;
-            double_t next_slope = 0;
-            double_t step_ms = step_size / (1000 * 1000.0);
-
-            if (get_index() == 0)
-                prev_slope = 1;
-            else {
-                if (throughput_target_fx[get_index() - 1].throughput_vec.size() == 0)
-                    re_configure(-1);
-                else {
-                    prev_throughput = calculate_throughput(throughput_target_fx[get_index() - 1]);
-                    prev_slope = (throughput - prev_throughput) / step_ms;
-                }
+        if (reconfigure_phase){
+            throughput_cnt++;
+            throughput_sum += cur_throughput;
+            if (throughput_cnt >= sliding_window_size){
+                reconfigure_phase = false;
+                base_throughput = throughput_sum / (throughput_cnt * 1.0);
+                throughput_sum = 0;
+                throughput_cnt = 0;
+                target_latency += step_size;
             }
-
-            if (get_index() == fx_size - 1)
-                next_slope = 0;
-            else {
-                if (throughput_target_fx[get_index() + 1].throughput_vec.size() == 0)
-                    re_configure(1);
-                else {
-                    next_throughput = calculate_throughput(throughput_target_fx[get_index() + 1]);
-                    next_slope = (next_throughput - throughput) / step_ms;
-                }
+        } else{
+//            auto normalized_target = (target_latency - min_target_latency);
+            auto normalized_throughput = cur_throughput - base_throughput;
+            if (normalized_throughput <= 0){
+                target_latency += step_size;
+            } else{
+                auto new_normalized_target = normalized_throughput / beta;
+                target_latency = min_target_latency + new_normalized_target;
             }
-            if (!reconfigure)
-                if (prev_slope > 0 && next_slope >= 0) {
-                    if (prev_slope > next_slope) {
-                        if (next_slope > beta && selected_target_index < fx_size - 1) {
-                            if (get_index() > 0)
-                                throughput_target_fx[get_index() - 1].throughput_vec.clear();
-                            selected_target_index++;
-                        } else if (prev_slope < beta && selected_target_index > 0) {
-                            if (get_index() < fx_size - 1)
-                                throughput_target_fx[get_index() + 1].throughput_vec.clear();
-                            selected_target_index--;
-                        }
-                    } else {
-                        re_configure(-1)
-                    }
-                } else {
-                    re_configure(-1)
-                }
         }
+        target_latency = std::max(target_latency, min_target_latency);
+        target_latency = std::min(target_latency, max_target_latency);
 
+        slow_interval_throughput = cur_throughput;
+        slow_interval_lat = (sum_latency / (1000 * 1000.0)) / slow_interval_txc_cnt;
     }
-
-
     slow_interval_start = mono_clock::now();
     coarse_interval_size = 0;
     sum_latency = 0;
@@ -197,7 +133,7 @@ void CoDel::_coarse_interval_process() {
 */
 bool CoDel::_check_latency_violation() {
     if (min_latency != INT_NULL) {
-        if (min_latency > calculate_target())
+        if (min_latency > target_latency)
             return true;
     }
     return false;
@@ -217,11 +153,13 @@ void CoDel::_update_interval() {
 void CoDel::reset() {
     std::lock_guard l(register_lock);
     interval = initial_interval;
+    target_latency = min_target_latency;
     min_latency = INT_NULL;
     coarse_interval_size = 0;
     slow_interval_start = mono_clock::zero();
-    slow_interval_throughput = 0;
-    slow_interval_lat = 0;
+    reconfigure_phase = true;
+    throughput_sum = 0;
+    throughput_cnt = 0;
     std::cout << "slow freq:" << slow_interval_frequency << std::endl;
     std::cout << "adaptive:" << adaptive_target << std::endl;
 }
