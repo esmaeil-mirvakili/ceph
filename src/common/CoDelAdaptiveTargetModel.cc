a1a62cd97ac4afea2847fdeff885360822530809
Esmaeil <smirvaki@ucsc.edu>
2021-08-23 11:37:20 -0700

    Config phase removed

--- src/common/CoDelAdaptiveTargetModel.cc
+++ src/common/CoDelAdaptiveTargetModel.cc
@@ -114,7 +114,7 @@ void CoDel::_coarse_interval_process() {
                 case NORMAL_PHASE: {
 //                    logfile << "1.5" << std::endl;
 //                    logfile.flush();
-                    if (slow_target_vec.size() >= size_threshold) {
+                    if (slow_target_vec.size() > size_threshold) {
                         slow_target_vec.erase(slow_target_vec.begin());
                         slow_throughput_vec.erase(slow_throughput_vec.begin());
                     }
@@ -124,13 +124,18 @@ void CoDel::_coarse_interval_process() {
                     std::vector<double> throughputs;
 //                    logfile << "1.7" << std::endl;
 //                    logfile.flush();
-                    double theta[2];
-                    CoDelUtils::log_fit(slow_target_vec, slow_throughput_vec, theta, outlier_detection);
+
 //                    logfile << "1.8" << std::endl;
 //                    logfile.flush();
-                    double target = (theta[1] / beta);
+                    double target = initial_target_latency;
 //                    logfile << "1.9" << std::endl;
 //                    logfile.flush();
+                    if(slow_target_vec.size() >= size_threshold){
+                        double theta[2];
+                        CoDelUtils::log_fit(slow_target_vec, slow_throughput_vec, theta, outlier_detection);
+                        target = (theta[1] / beta);
+                    }
+
                     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
                     std::default_random_engine generator(seed);
                     double dist_params[2];
