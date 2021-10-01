b4a1c55c5c47ea6e5b46ccd431c56c71cae3a2e3
Esmaeil <smirvaki@ucsc.edu>
2021-10-01 15:42:58 -0700

    SlowFastCommit added

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1840,9 +1840,6 @@ public:
     }
   } throttle;
 
-  class SocketHook;
-  SocketHook *asok_hook = nullptr;
-
   /**
    * CoDel algorithm to calculate the incoming batch size to kv by using Throttle mechanism
    */
@@ -2174,6 +2171,9 @@ private:
   int path_fd = -1;  ///< open handle to $path
   int fsid_fd = -1;  ///< open handle (locked) to $path/fsid
   bool mounted = false;
+
+  class SocketHook;
+  SocketHook *asok_hook = nullptr;
 
   ceph::shared_mutex coll_lock = ceph::make_shared_mutex("BlueStore::coll_lock");  ///< rwlock to protect coll_map
   mempool::bluestore_cache_other::unordered_map<coll_t, CollectionRef> coll_map;
