f4eb43e607079dc2251bb2db9bae760c3f999c7c
esmaeil-mirvakili <smirvaki@ucsc.edu>
2025-03-09 15:40:39 -0700

    bug fixed

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -56,6 +56,7 @@
 #include "osd/osd_perf_counters.h"
 #include "common/Finisher.h"
 #include "scrubber/osd_scrub.h"
+#include "common/admin_socket.h"
 #include "common/DataCollectionService.h"
 
 #define CEPH_OSD_PROTOCOL    10 /* cluster internal */
@@ -1060,6 +1061,58 @@ struct OSDShard {
     OSD *osd,
     op_queue_type_t osd_op_queue,
     unsigned osd_op_queue_cut_off);
+};
+
+class DataSocketHook : public AdminSocketHook
+{
+    CephContext *cct;
+    OSD *osd;
+
+public:
+    static DataSocketHook *create(CephContext *cct_, OSD *osd)
+    {
+      DataSocketHook *hook = nullptr;
+      AdminSocket *admin_socket = cct_->get_admin_socket();
+      if (admin_socket)
+      {
+        hook = new DataSocketHook(cct_, osd);
+        int r = admin_socket->register_command("start data collection",
+                                               hook,
+                                               "start collecting data");
+        r = admin_socket->register_command("stop data collection",
+                                           hook,
+                                           "stop and reset data collection");
+        if (r != 0)
+        {
+          delete hook;
+          hook = nullptr;
+        }
+      }
+      return hook;
+    }
+    ~DataSocketHook()
+    {
+      AdminSocket *admin_socket = cct->get_admin_socket();
+      admin_socket->unregister_commands(this);
+    }
+
+private:
+    DataSocketHook(CephContext *cct_, OSD *osd) : cct(cct_), osd(osd) {}
+    int call(std::string_view command, const cmdmap_t &cmdmap,
+             const bufferlist &in,
+             Formatter *f,
+             std::ostream &ss,
+             bufferlist &out) override
+    {
+      if (command == "start data collection")
+      {
+        osd->dataCollectionService.start();
+      } else if (command == "stop data collection") {
+        osd->dataCollectionService.stop();
+        osd->dataCollectionService.dump();
+      }
+      return 0;
+    }
 };
 
 class OSD : public Dispatcher,
